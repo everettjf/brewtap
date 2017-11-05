ade447ab43c8ffbe9f0c2513f897b79f2bfd9ef8
everettjf <everettjf@live.com>
2017-11-05 23:05:02 +0800

    test init

---
+++ Formula/moex.rb
@@ -0,0 +1,15 @@
+class Notes < Formula
+    desc "CLI tool for quick explore mach-o file"
+    homepage "https://github.com/everettjf/MachOExplorer"
+    url "https://github.com/kylebebak/notes/archive/1.0.1.tar.gz"
+    version "0.3.0"
+    sha256 "a06a2e6bd8a7d5d476644a9343f0dc4686b58151007c80c34f733024c16eca67"
+  
+    def install
+      bin.install "bin/notes"
+      prefix.install Dir["_completions"]
+      prefix.install Dir["_helpers"]
+      prefix.install Dir["_config"]
+    end
+  end
+  
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# brewtap
+# homebrew-tap
+
+## How to install 
+
+```
+brew tap everettjf/tap
+```
+
+## Tools
+
+### moex
+
+```
+brew install moex
+```
