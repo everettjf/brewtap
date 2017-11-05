73071c53ec9c1774c75eec3916c46537b5320c50
everettjf <everettjf@live.com>
2017-11-05 23:52:16 +0800

    moex

--- Formula/moex.rb
+++ Formula/moex.rb
@@ -1,15 +1,12 @@
 class Moex < Formula
     desc "CLI tool for quick explore mach-o file"
     homepage "https://github.com/everettjf/MachOExplorer"
-    url "https://github.com/kylebebak/notes/archive/1.0.1.tar.gz"
-    version "0.3.0"
-    sha256 "a06a2e6bd8a7d5d476644a9343f0dc4686b58151007c80c34f733024c16eca67"
+    url "https://github.com/everettjf/MachOExplorer/releases/download/v0.4.0/moex.tar.gz"
+    version "0.4.0"
+    sha256 "260021d5a3efd48838be38e20f420852810cc8348814b37f243cc77cf7cf7a35"
   
     def install
-      bin.install "bin/notes"
-      prefix.install Dir["_completions"]
-      prefix.install Dir["_helpers"]
-      prefix.install Dir["_config"]
+      bin.install "bin/moex"
     end
   end
   
